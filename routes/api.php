547a72d269c5b99c7fad468f6f0fc1524020a5d8
unknown <simon.frezard@gmail.com>
2021-03-25 16:09:39 +0100

    migrated tables from passport, installed passport, made routes for Passport in AuthServiceProvider, made Route for user for the api to login, register & logout and details from middleware

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'auth'], function(){
+    Route::post('login', [MainController::class, 'login']);
+    Route::post('register', [MainController::class, 'register']);
+
+    Route::group(['middleware' => 'auth:api'], function(){
+        Route::get('logout', [MainController::class, 'logout']);
+        Route::get('detail', [MainController::class, 'detail']);
+    });
 });
