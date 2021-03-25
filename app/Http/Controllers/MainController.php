807ca78a875fad087c95701b8924f7ac870a5ee6
unknown <simon.frezard@gmail.com>
2021-03-25 17:32:07 +0100

    added login method, used postman, getting our token

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,86 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function register(Request $request){
         $request->validate([
             'name' => 'required | string',
@@ -100,5 +26,30 @@ class MainController extends Controller
         return response()->json([
             'message' => 'This worked, a user was created'
         ], 201);
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email' => 'required | string | email',
+            'remember_me' => 'boolean'
+        ]);
+        $userCredentials = request(['email', 'password']);
+        if (!Auth::attempt($userCredentials))
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+            $user = $request->user();
+            $tokenResult = $user->createToken('User Personal Access Token');
+            $token = $tokenResult->token;
+
+            if ($request->remember_me){
+                $token->expires_at = Carbon::now()->addWeek(2);
+            }
+            $token->save();
+            return response()->json([
+                'access_token' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expires_at' => Carbon::parse($tokenResult->token->expires_at)->toDateString()
+            ]);
     }
 }
